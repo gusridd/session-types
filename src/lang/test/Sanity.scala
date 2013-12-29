1e7b519649b53156d98de40ed58b88b6846c03c0
Gustavo Soto Ridd <tatacarrera@gmail.com>
2013-12-29 01:58:01 -0300

    added tests for threadReduction rules\nremoved tests for the bit alternating protocol as is suspected to be non-reductible

--- src/lang/test/Sanity.scala
+++ src/lang/test/Sanity.scala
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import lang._
 import java.io.FileReader;
+import java.io.StringReader;
 
 class Sanity {
 
@@ -62,15 +63,62 @@ class Sanity {
     reader2.close
     g2.threadReduction()
     
-    val reader3 = new FileReader("./src/lang/test/AlternatingBitProtocol.txt")
-    val g3: GlobalProtocol = GlobalParser.parse(reader3)
-    reader3.close
-    g3.threadReduction()
+//    val reader3 = new FileReader("./src/lang/test/AlternatingBitProtocol.txt")
+//    val g3: GlobalProtocol = GlobalParser.parse(reader3)
+//    reader3.close
+//    g3.threadReduction()
 //    
 //    val reader4 = new FileReader("./src/lang/test/AlternatingBitProtocol3.txt")
 //    val g4: GlobalProtocol = GlobalParser.parse(reader4)
 //    reader4.close
 //    g4.threadReduction()
+    
+    val reader5 = new FileReader("./src/lang/test/interleavedChoice.txt")
+    val g5: GlobalProtocol = GlobalParser.parse(reader5)
+    reader5.close
+    g5.threadReduction()
+    
+    val reader6 = new FileReader("./src/lang/test/interleavedSAndTSystem.txt")
+    val g6: GlobalProtocol = GlobalParser.parse(reader6)
+    reader6.close
+    g6.threadReduction()
+  }
+  
+  @Test
+  def testThreadRulesTrans(){
+    val protocol = "x_0 = A -> B : L(U); x_1"
+    val reader = new StringReader(protocol)
+    val g: GlobalProtocol = GlobalParser.parse(reader)
+    g.threadReduction()
+  }
+  
+  @Test
+  def testThreadRulesPar(){
+    val protocol = "x_1 = x_2 | x_3 \n x_2 | x_3 = x_4"
+    val reader = new StringReader(protocol)
+    val g: GlobalProtocol = GlobalParser.parse(reader)
+    g.threadReduction()
+  }
+  
+  @Test
+  def testThreadRulesBra(){
+    val protocol = "x_1 = x_2 + x_3 \n x_2 + x_3 = x_4"
+    val reader = new StringReader(protocol)
+    val g: GlobalProtocol = GlobalParser.parse(reader)
+    g.threadReduction()
+  }
+  
+  @Test
+  def testThreadRulesRec(){
+    val protocol1 = "x_1 + x_2 = x_3 \n x_3 = x_4 + x_2"
+    val reader1 = new StringReader(protocol1)
+    val g1: GlobalProtocol = GlobalParser.parse(reader1)
+    g1.threadReduction()
+    
+    val protocol2 = "x_4 + x_2 = x_3 \n x_3 = x_2 + x_1"
+    val reader2 = new StringReader(protocol2)
+    val g2: GlobalProtocol = GlobalParser.parse(reader2)
+    g2.threadReduction()
   }
   
   @Test
