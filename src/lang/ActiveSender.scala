9e00ce218372111cf30a44574c30d9b71928979e
Gustavo Soto Ridd <tatacarrera@gmail.com>
2014-04-12 19:55:33 -0300

    ActiveSender with mandatory conmutation

--- src/lang/ActiveSender.scala
+++ src/lang/ActiveSender.scala
@@ -26,7 +26,7 @@ object ActiveSender {
          */
         implicit val justConmuted = false
         val activeSenders = g.getParticipants map 
-        (p => (p,reduce(g, x1, Set(), Set(p), x2, Set(), Set(p)))) filter (e => e._2)
+        (p => (p,reduce(g, x1, Set(), Set(p), x2, Set(), Set(p),0))) filter (e => e._2)
         if (activeSenders.size != 1){
           throw new NoActiveSenders(x)
         } else {
@@ -38,28 +38,31 @@ object ActiveSender {
   }
   def reduce(g: GlobalProtocol, actL: String, setL: Set[String],
     parL: Set[String], actR: String, setR: Set[String],
-    parR: Set[String])(implicit justConmuted : Boolean) : Boolean = {
+    parR: Set[String], counter : Int) : Boolean = {
     val (leftHash, rightHash) = g.getHashes
+    val el = leftHash(actL)
+    println(actL + " " + el)
     leftHash(actL) match {
+      /* Base cases */
       case End(x) if actL == actR => true
       case End(x) if setR.contains(actR) => true
-      case ChoiceJoin(x1,x2,x) if x1 == actL && x2 == actR => true
       case _ if (setL.contains(actL) && setR.contains(actR)) => true
-      case ParallelJoin(x1,x2,xp) if (actL == x1 || actL == x2) 
-        && reduce(g,xp,setL,parL,actR,setR,parR)(false) => true
-      case ChoiceJoin(x1,x2,xp) if (actL == x1 || actR == x2)
-        && reduce(g,xp,setL,parL,actR,setR,parR)(false) => true
+      case ChoiceJoin(x1,x2,x) if (x1 == actL && x2 == actR) || (x2 == actL && x1 == actR) => true
+      case _ if (counter == 3) => reduce(g, actR, setR, parR, actL,setL,parL,0)
+      /* Recursive cases */
+      case ParallelJoin(x1,x2,xp) if reduce(g,xp,setL,parL,actR,setR,parR,counter+1) => true
+      case ChoiceJoin(x1,x2,xp) if reduce(g,xp,setL,parL,actR,setR,parR,counter+1) => true
       case Parallel(x1,x,xp) 
-      if reduce(g,x,setL+x1,parL,actR,setR,parR)(false) 
-        && reduce(g,xp,setL+x1,parL,actR,setR,parR)(false) => true
-      case Choice(x1,x,xp) if reduce(g,x,setL+x1,parL,actR,setR,parR)(false)
-        && reduce(g,xp,setL+x1,parL,actR,setR,parR)(false) => true
+      if reduce(g,x,setL+x1,parL,actR,setR,parR,counter+1) 
+        && reduce(g,xp,setL+x1,parL,actR,setR,parR,counter+1) => true
+      case Choice(x1,x,xp) if reduce(g,x,setL+x1,parL,actR,setR,parR,counter+1)
+        && reduce(g,xp,setL+x1,parL,actR,setR,parR,counter+1) => true
       case Message(x1,p,pp,_,_,x1p) if parL.contains(p) 
-        && reduce(g,x1p,setL+x1,parL+pp,actR,setR,parR)(false) => true
+        && reduce(g,x1p,setL+x1,parL+pp,actR,setR,parR,counter+1) => true
       case _ => {
-        if(justConmuted) false
+        if(counter == 0) false
         else {
-          reduce(g, actR, setR, parR, actL,setL,parL)(true)
+          reduce(g, actR, setR, parR, actL,setL,parL,0)
         }
       }
     }
