5f1c8d4c467eb66ceda422e84dd5093a5466ba76
Gustavo Soto Ridd <tatacarrera@gmail.com>
2014-04-20 01:53:29 -0300

    implemented local proyection algorithm resolve #6

--- src/lang/WFGlobalProtocol.scala
+++ src/lang/WFGlobalProtocol.scala
@@ -1,5 +1,7 @@
 package lang
 
+import LocalProtocol._
+
 class WFGlobalProtocol(exprs: List[expr]) extends GlobalProtocol(exprs) {
   /**
    * Any Well-formed Global Protocol must comply with the sanity, local
@@ -8,7 +10,37 @@ class WFGlobalProtocol(exprs: List[expr]) extends GlobalProtocol(exprs) {
   Sanity(this)
   LocalChoice(this)
   Linearity(this)
-  
-  
-  
+
+  def localProjection(p: String): Iterable[localExpr] = {
+    getParticipants().find(_ == p) match {
+      case Some(pp) => lp(p)
+      case None =>
+        throw new Exception("Trying to project a non-existant participant " + p)
+    }
+  }
+
+  private def lp(participant: String): Iterable[localExpr] = {
+    exprs map (e => e match {
+      case Message(x, p, pp, l, t, xp) if (participant == p) => Send(x, pp, l, t, xp)
+      case Message(x, p, pp, l, t, xp) if (participant == pp) => Receive(x, p, l, t, xp)
+      case Message(x, p, pp, l, t, xp) => Indirection(x, xp)
+      case ParallelJoin(x, xp, xpp) => Join(x, xp, xpp)
+      case Parallel(x, xp, xpp) => Fork(x, xp, xpp)
+      case Choice(x, xp, xpp) => {
+        try {
+          val as = ActiveSender(this, x)
+          if (participant == as) {
+            InternalChoice(x, xp, xpp)
+          } else {
+            ExternalChoice(x, xp, xpp)
+          }
+        } catch {
+          case e: Exception => ExternalChoice(x, xp, xpp)
+        }
+      }
+      case ChoiceJoin(x, xp, xpp) => Merge(x, xp, xpp)
+      case End(x) => LocalProtocol.End(x)
+    })
+  }
+
 }
---
+++ src/lang/test/LocalProtocol.scala
@@ -0,0 +1,52 @@
+package lang.test;
+
+import org.junit.Assert._
+import org.junit.Test
+import lang._
+import lang.LocalProtocol._
+import java.io.{FileReader => FR}
+import java.io.{StringReader => SR}
+import java.io.Reader
+
+class LocalProtocol {
+
+  val path = "./src/lang/test/"
+  val path_wf = "./src/protocol/wellformed/"
+
+  def getProtocol(reader: Reader) = {
+    val g: GlobalProtocol = GlobalParser.parse(reader)
+    reader.close
+    val wf = new WFGlobalProtocol(g.exprs)
+    wf
+  }
+
+  @Test def testHelloWorld() {
+    val g = getProtocol(new FR(path_wf + "HelloWorld.txt"))
+    
+    val localMe = g.localProjection("Me").toSet
+    val localWorld = g.localProjection("World").toSet
+    
+    assertTrue(localMe.contains(Send("x_0","World","Say","Hello","x_1")))
+    assertTrue(localMe.contains(LocalProtocol.End("x_1")))
+    
+    assertTrue(localWorld.contains(Receive("x_0","Me","Say","Hello","x_1")))
+    assertTrue(localWorld.contains(LocalProtocol.End("x_1")))
+    
+  }
+  
+  @Test def testOnlineBookStore() {
+    val g = getProtocol(new FR(path_wf + "OnlineBookstore.txt"))
+  }
+  
+  @Test def testPostOffice() {
+    val g = getProtocol(new FR(path_wf + "PostOffice.txt"))
+  }
+  
+  @Test def testTravelAgency() {
+    val g = getProtocol(new FR(path_wf + "TravelAgency.txt"))
+  }
+
+
+
+
+}
