34b26147b6f54b5fd7641dd3f396768d7efcefc7
Gustavo Soto Ridd <tatacarrera@gmail.com>
2014-04-19 22:47:57 -0300

    not that weird

--- src/lang/WellFormedGlobalProtocol.scala
+++
@@ -1,9 +0,0 @@
-package lang
-
-import lang.LocalChoice
-
-import lang.wellformedness.ellFormedGlobalProtocol(exprs: List[expr]) extends GlobalProtocol(exprs) {
-	Sanity(this)
-	LocalChoice(this)
-
-}
