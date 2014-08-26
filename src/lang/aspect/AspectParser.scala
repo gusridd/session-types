b3022e305bc65a917306d24050236640b49f0e7b
Gustavo Soto Ridd <gsoto@dcc.uchile.cl>
2014-08-26 09:45:11 +0200

    starting to add aspect support by the means of a parser

---
+++ src/lang/aspect/AspectParser.scala
@@ -0,0 +1,21 @@
+package lang.aspect
+
+import scala.util.parsing.combinator.JavaTokenParsers
+import scala.util.parsing.input.Positional
+
+class AspectParser extends JavaTokenParsers {
+
+  
+  
+  
+}
+
+
+
+
+/**
+ * AST definitions for the AspectParser
+ */
+
+
+sealed trait aspectAST extends Positional
