7ee7dffd2f42fa554e1729df24e19da6faf2f945
Gustavo Soto Ridd <gsoto@dcc.uchile.cl>
2014-08-29 11:10:06 +0200

    added global weaving tests

--- src/test/lang/aspect/Weaver.scala
+++ src/test/lang/aspect/Weaver.scala
@@ -13,19 +13,28 @@ import lang.expr
 class Weaver extends PathInfo {
   import lang.aspect.Weaver
   
-  @Test def testParseAuthentication() {
+  @Test def testWeaveAuthenticationWithSimpleTrade() {
     val aspects = AspectParser.parse(new FR(path_wf_a + "Authentication.txt"))
     val protocol = GlobalParser.parse(new FR(path_wf + "SimpleTrade.txt"))
     val wovenType = Weaver.naiveGlobalWeaving(aspects, protocol.exprs)
     wovenType map (x => println(x.canonical))
   }
   
-  @Test def testParseLogging() {
-    val aspect = AspectParser.parse(new FR(path_wf_a + "Logging.txt"))
+  @Test def testWeaveLoggingWithSimpleTrade() {
+    val aspects = AspectParser.parse(new FR(path_wf_a + "Logging.txt"))
+    val protocol = GlobalParser.parse(new FR(path_wf + "SimpleTrade.txt"))
+    val wovenType = Weaver.naiveGlobalWeaving(aspects, protocol.exprs)
+    wovenType map (x => println(x.canonical))
   }
   
-  @Test def testParseNegotiation() {
-    val aspect = AspectParser.parse(new FR(path_wf_a + "Negotiation.txt"))
+  @Test def testWeaveNegotiationWithSimpleTrade() {
+    val aspects = AspectParser.parse(new FR(path_wf_a + "Negotiation.txt"))
+    println(aspects)
+    val protocol = GlobalParser.parse(new FR(path_wf + "SimpleTrade.txt"))
+    println(protocol)
+    val wovenType = Weaver.naiveGlobalWeaving(aspects, protocol.exprs)
+    println(wovenType)
+    wovenType map (x => println(x.canonical))
   }
 
 }
