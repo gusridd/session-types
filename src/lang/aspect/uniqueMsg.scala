4d2b7ff3d7339cd9391f1e4bb4239e4c8e67894e
Gustavo Soto Ridd <tatacarrera@gmail.com>
2014-09-01 23:25:19 +0200

    definitions and test for disjointPartition and sumPartition

--- src/lang/aspect/uniqueMsg.scala
+++ src/lang/aspect/uniqueMsg.scala
@@ -1,52 +1,70 @@
 package lang.aspect
 
-import lang.GlobalProtocol
+import scala.Option.option2Iterable
 import lang.Message
+import lang.Choice
+import lang.Parallel
+import lang.End
 
 object uniqueMsg {
   /**
-   * Main method that tries to check 
+   * Main method that tries to check
    */
   def apply(a: Aspect): Boolean = {
-    
+
     val (leftHash, rightHash) = a.adv.getHashes
-    def Msg(x: String, xb: Set[String], M: Set[Message]): Boolean = {
-    	leftHash(x) match {
-    	  case m @ Message(x,_,_,_,_,xp) => {
-    	    val Mp = M - m
-    	    // The disjoint sum of sets considers the empty set
-    	    Msg(xp,xb,Mp) || Msg(xp,xb,M)
-     	  }
-    	  case AdviceTransition(x1,x2) => {
-    	    val pairs = (M.subsets map (s => (s,M--s)))
-    	    
-    	    // This represent all the disjoint sums in whose all the elements
-    	    // of the first set are matched by some pointcut.
-    	    val properties = pairs filter({
-    	      case (sM,sMP) => sM forall (m => Weaver.pointcutMatchGlobal(a.pc,m))
-    	    })
-    	    properties exists({
-    	      case (m,mp) => Msg(x2,xb,mp)
-    	    })
-    	  }
-    	}
-    }
-    
+      def Msg(x: String, xb: Set[String], M: Set[Message]): Boolean = {
+        leftHash(x) match {
+          case m @ Message(x, _, _, _, _, xp) => {
+            val Mp = M - m
+            // The disjoint sum of sets considers the empty set
+            Msg(xp, xb, Mp) || Msg(xp, xb, M)
+          }
+          case AdviceTransition(x1, x2) => {
+            val pairs = (M.subsets map (s => (s, M -- s)))
+
+            // This represent all the disjoint sums in whose all the elements
+            // of the first set are matched by some pointcut.
+            val properties = pairs filter ({
+              case (sM, sMP) => sM forall (m => Weaver.pointcutMatchGlobal(a.pc, m))
+            })
+            properties exists ({
+              case (m, mp) => Msg(x2, xb, mp)
+            })
+          }
+          case Choice(x, x1, x2) if (!xb.contains(x)) => false
+
+          case Choice(x, x1, x2) if (xb.contains(x)) => true
+          case Parallel(x, x1, x2) if (xb.contains(x)) => true
+          case End(x) => true
+        }
+      }
+
     /**
      * We try to find to start the computation assuming that every message
      * could be unique as we don't know from the start which one could be.
      */
     val uniqueMessages = a.adv.exprs flatMap {
-      case m@Message(_,_,_,_,_,_) => Some(m)
+      case m @ Message(_, _, _, _, _, _) => Some(m)
       case _ => None
-    } map (M => Msg("x_0",Set(),Set(M)))
-    
+    } map (M => Msg("x_0", Set(), Set(M)))
+
     uniqueMessages.size == 1
   }
   
-  def partition(s: Set[String]): (Set[String],Set[String]) = {
-    val subs = s.subsets
-    subs foreach (sub => println(sub + "\t\t" + (s.--(sub)).toString))
-    (Set(),Set())
+  type iSet[T] = scala.collection.immutable.Set[T]
+
+  def disjointPartition(s: Set[Message]): Iterator[(iSet[Message],iSet[Message])] =
+    for {
+      s1 <- s.subsets
+    } yield (s1, s -- s1)
+  
+  def sumPartition(s: Set[Message]): Iterator[(iSet[Message],iSet[Message])] = {
+    for {
+      s1 <- s.subsets
+      s2 <- s.subsets
+      if (s1 ++ s2 == s)
+    } yield (s1, s2)
   }
+    
 }
--- src/test/lang/aspect/UniqueMsg.scala
+++ src/test/lang/aspect/UniqueMsg.scala
@@ -8,13 +8,46 @@ import java.io.{ StringReader => SR }
 import test.lang.PathInfo
 import lang.GlobalParser
 import lang.expr
+import lang.Message
 
 
 class UniqueMsg extends PathInfo {
   import lang.aspect.uniqueMsg
   
-  @Test def disjointSet(){
-    val sets = uniqueMsg.partition(Set("A","B","C","D"))
+  @Test def disjointPartition(){
+    val set = Set(
+        Message("","A","","","",""),
+        Message("","B","","","",""),
+        Message("","C","","","",""),
+        Message("","D","","","",""))
+    val sets = uniqueMsg.disjointPartition(set)
+    
+    sets foreach {
+      case (s1,s2) => {
+        println(s1.toString + " U " + s2.toString)
+        // The sum of the subsets should be the initial set
+        assertEquals(set, s1 ++ s2)
+        // The set intersection should be the empty set
+        assertEquals(Set(),s1.intersect(s1))
+      }
+    }
+  }
+  
+  @Test def sumPartition() {
+    val set = Set(
+        Message("","A","","","",""),
+        Message("","B","","","",""),
+        Message("","C","","","",""),
+        Message("","D","","","",""))
+    
+    val sets = uniqueMsg.sumPartition(set)
+    sets foreach {
+      case (s1,s2) => {
+//        println(s1.toString + " U " + s2.toString)
+        // The sum of the subsets should be the initial set
+        assertEquals(set, s1 ++ s2)
+      }
+    }
   }
   
 }
