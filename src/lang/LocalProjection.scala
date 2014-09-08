70eb526f45f8eb60519ebedcd74adbdf3d92f28e
Gustavo Soto Ridd <gsoto@dcc.uchile.cl>
2014-09-08 10:56:45 +0200

    added NullAction to localProtoocls

---
+++ src/lang/LocalProjection.scala
@@ -0,0 +1,54 @@
+package lang
+
+import lang.LocalProtocol.Send
+import lang.LocalProtocol.Receive
+import lang.LocalProtocol.Send
+import lang.LocalProtocol.Join
+import lang.LocalProtocol.Fork
+import lang.LocalProtocol.InternalChoice
+import lang.LocalProtocol.ExternalChoice
+import lang.LocalProtocol.Merge
+
+object LocalProjection {
+  def apply(g: GlobalProtocol, p: String) = {
+    g.getParticipants().find(_ == p) match {
+      //TODO: change this to a fold
+      case Some(pp) => {
+        var localProtocol = lp(g, p)
+        localProtocol foreach ({
+          case i @ LocalProtocol.Indirection(x1, x2) => {
+            localProtocol = (localProtocol filterNot (_ == i)).map(_.substitute(x1, x2))
+          }
+          case _ =>
+        })
+        localProtocol
+      }
+      case None =>
+        throw new Exception("Trying to project a non-existant participant " + p)
+    }
+  }
+
+  private def lp(g: GlobalProtocol, participant: String): Iterable[LocalProtocol.localExpr] = {
+    g.exprs map (e => e match {
+      case Message(x, p, pp, l, t, xp) if (participant == p) => Send(x, pp, l, t, xp)
+      case Message(x, p, pp, l, t, xp) if (participant == pp) => Receive(x, p, l, t, xp)
+      case Message(x, p, pp, l, t, xp) => LocalProtocol.Indirection(x, xp)
+      case ParallelJoin(x, xp, xpp) => Join(x, xp, xpp)
+      case Parallel(x, xp, xpp) => Fork(x, xp, xpp)
+      case Choice(x, xp, xpp) => {
+        try {
+          val as = ActiveSender(g, x)
+          if (participant == as) {
+            InternalChoice(x, xp, xpp)
+          } else {
+            ExternalChoice(x, xp, xpp)
+          }
+        } catch {
+          case e: Exception => ExternalChoice(x, xp, xpp)
+        }
+      }
+      case ChoiceJoin(x, xp, xpp) => Merge(x, xp, xpp)
+      case End(x) => LocalProtocol.End(x)
+    })
+  }
+}
