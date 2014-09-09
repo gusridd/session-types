be25c59b352489b4d4f79719d54e1c3daae5bbfe
Gustavo Soto Ridd <gsoto@dcc.uchile.cl>
2014-09-09 17:41:48 +0200

    new implementation for Lin

--- src/lang/Linearity.scala
+++ src/lang/Linearity.scala
@@ -9,31 +9,31 @@ object Linearity {
     })
   }
 
-  class UndefinedLinearityException(xt: List[String], pt: List[String], xi: String) extends Exception {
+  class UndefinedLinearityException(xt: List[String], xtt: List[String], pt: List[String], xi: String) extends Exception {
     override def toString: String =
       "UndefinedLinearityException: Undefined Lin(G," + xt + "," + pt + ")(" + xi + ")"
   }
 
   def lin(g: GlobalProtocol)(x: String) = {
-    new ReceiverOutput(l(g, List(), List(), x))
+    new ReceiverOutput(l(g, List(), List(), List(), x))
   }
 
-  private def l(g: GlobalProtocol, xt: List[String], pt: List[String], xi: String): Set[(String, String, List[String])] = {
-    val it = g.exprs.iterator
-    while (it.hasNext) {
-      val e = it.next
-      e match {
-        case ChoiceJoin(x, xp, xpp) if (xi == x || xi == xp) && xt.contains(xpp) => return Set.empty
-        case End(x) if (xi == x) => return Set.empty
-        case Message(x, p, pp, m, u, xp) if (xi == x) && !pt.contains(pp) => return Set((pp, m, List(xi))) ++ l(g, xt, pt, xp)
-        case Choice(x, xp, xpp) if (xi == x) => return l(g, xt, pt, xp) ++ l(g, xt, pt, xpp)
-        case Parallel(x, xp, xpp) if (xi == x) => return l(g, xt, pt, xp) ++ l(g, xt, pt, xpp)
-        case ChoiceJoin(x, xp, xpp) if (xi == x || xi == xp) && !xt.contains(xpp) => return l(g, xt :+ xpp, pt, xpp)
-        case ParallelJoin(xp, x, xpp) if (xi == x || xi == xp) => return l(g, xt, pt :+ xpp, xpp)
-        case _ => return Set.empty
-      }
+  private def l(g: GlobalProtocol, xm: List[String], xj: List[String], pt: List[String], xi: String): Set[(String, String, List[String])] = {
+    val (lHash, rHash) = g.getHashes
+    val e = lHash(xi)
+    println("Lin(G," + xm + ", " + pt + ")(" + xi + ")")
+    println(e)
+    e match {
+      case ChoiceJoin(x, xp, xpp) if (xm.contains(xpp)) => Set()
+      case End(x) => Set()
+      case Message(x, p, pp, m, u, xp) if (!pt.contains(pp)) => Set((p + pp, m, xj)) ++ l(g, xm, xj, pt :+ pp, xp)
+      case Message(x, p, pp, m, u, xp) => l(g, xm, xj, pt, xp)
+      case Choice(x, xp, xpp) => l(g, xm, xj, pt, xp) ++ l(g, xm, xj, pt, xpp)
+      case Parallel(x, xp, xpp) => l(g, xm, xj, pt, xp) ++ l(g, xm, xj, pt, xpp)
+      case ChoiceJoin(x, xp, xpp) if (!xm.contains(xpp)) => l(g, xm :+ xpp, xj, pt, xpp)
+      case ParallelJoin(x,xp,xpp) => l(g,xm,xj :+ xpp,pt,xpp)
+      case _ => throw new UndefinedLinearityException(xm, xj, pt, xi)
     }
-    // This will happen if the protocol is incomplete
-    throw new UndefinedLinearityException(xt, pt, xi)
+
   }
 }
