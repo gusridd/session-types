def3837ce296106ecf3f772cad31342edf4188d6
Gustavo Soto Ridd <gsoto@dcc.uchile.cl>
2014-09-10 10:09:32 +0200

    Congruence reduction deals with x_0 state

--- src/lang/Congruence.scala
+++ src/lang/Congruence.scala
@@ -4,6 +4,12 @@ package lang
  * The methods listed here do not correspond to a congruence relation, but
  * to a congruence reduction. The basis is to take a list of expr and replace
  * all Indirection and NullAction, using the substitute method provided by expr.
+ *
+ * This also deals with the fact that all protocols start with the state "x_0".
+ * If a protocol starts with an Indirection the substitute rules are different
+ * so the state "x_0" is preserved. Example:
+ * Typically x_3 = x_5 would replace all occurrences of x_3 for x_5, but in the
+ * case x_0 = x_2 the replacement is reversed so x_2 is replaced by x_0
  */
 object Congruence {
 
@@ -13,6 +19,12 @@ object Congruence {
 
   def apply(exprs: Iterable[lang.expr]): Iterable[lang.expr] = {
     exprs.foldLeft(exprs)((old: Iterable[expr], curr: expr) => curr match {
+      case i @ LocalProtocol.Indirection("x_0", x2) => {
+        (old filterNot (_ == i)).map(_.substitute(x2, "x_0"))
+      }
+      case i @ Indirection("x_0", x2) => {
+        (old filterNot (_ == i)).map(_.substitute(x2, "x_0"))
+      }
       case i @ LocalProtocol.Indirection(x1, x2) => {
         (old filterNot (_ == i)).map(_.substitute(x1, x2))
       }
--- src/test/lang/Congruence.scala
+++ src/test/lang/Congruence.scala
@@ -18,8 +18,8 @@ class Congruence {
     	  """))
     val reduced = Congruence(g)
     val ng = new GlobalProtocol(reduced.to)
-    
-    assertEquals(2,ng.exprs.size)
+
+    assertEquals(2, ng.exprs.size)
   }
 
   @Test def testCongruenceIndirectionWithParallel() {
@@ -34,8 +34,8 @@ class Congruence {
     	  """))
     val reduced = Congruence(g)
     val ng = new GlobalProtocol(reduced.to)
-    
-    assertEquals(4,ng.exprs.size)
+
+    assertEquals(4, ng.exprs.size)
   }
 
   @Test def testCongruenceIndirectionWithChoice() {
@@ -50,7 +50,29 @@ class Congruence {
     	  """))
     val reduced = Congruence(g)
     val ng = new GlobalProtocol(reduced.to)
+
+    assertEquals(4, ng.exprs.size)
+  }
+
+  @Test def testCongruenceNaiveWeaveSimpleTradeWithNegotiation() {
+    val exprs = List(
+      Message("x_0^[x_0]", "S", "B", "Item", "String", "x_1^[x_0)]"),
+      ChoiceJoin("x_1^[x_0]", "x_6^[x_0]", "x_2^[x_0]"),
+      Choice("x_2^[x_0]", "x_3^[x_0]", "x_4^[x_0]"),
+      Message("x_3^[x_0]", "B", "C", "Offer", "Int", "x_5^[x_0]"),
+      Message("x_5^[x_0]", "C", "B", "Counter", "Int", "x_6^[x_0]"),
+      Indirection("x_4^[x_0]", "x_1"),
+      Indirection("x_0", "x_0^[x_0]"),
+      Parallel("x_1", "x_2", "x_3"),
+      Message("x_2", "B", "S", "Sale", "Boolean", "x_4"),
+      Message("x_3", "B", "C", "Purchase", "Boolean", "x_5"),
+      ParallelJoin("x_4", "x_5", "x_6"),
+      End("x_6"))
+
+    val nExprs: Set[expr] = Congruence(exprs).to
+
+    assertEquals(10, nExprs.size)
     
-    assertEquals(4,ng.exprs.size)
+    assertTrue(nExprs.contains(Message("x_0","S","B","Item","String","x_1^[x_0)]")))
   }
 }
