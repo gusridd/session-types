364153adcd8fe4734a2fc51c82334ad715d1ca16
Gustavo Soto Ridd <gsoto@dcc.uchile.cl>
2014-09-11 09:14:34 +0200

    added new parameter to Rcv function, now it remembers the starting point so, if a cycle is involved it does not remember the merging path to the path that has nothing to do with the cycle in the first place.

--- src/lang/Receiver.scala
+++ src/lang/Receiver.scala
@@ -11,12 +11,12 @@ object Receiver {
 
   def apply(g: GlobalProtocol)(x: String) = {
     val cg = new GlobalProtocol(Congruence(g).to)
-    
+
     val (hLeft, hRight) = cg.getHashes
     hRight(x) match {
-      case Choice(xc, _, _) => {
-        val activeSender = ActiveSender(cg, xc)
-        val setWithoutActiveSender = r(cg, List(), Set(), x) filter {
+      case Choice(xch, _, _) => {
+        val activeSender = ActiveSender(cg, xch)
+        val setWithoutActiveSender = r(cg, xch, List(), Set(), x) filter {
           case (p, _, _) => p != activeSender
         }
         new ReceiverOutput(setWithoutActiveSender)
@@ -26,20 +26,21 @@ object Receiver {
 
   }
 
-  private def r(g: GlobalProtocol, xt: List[String], pt: Set[String], xi: String): Set[(String, String, List[String])] = {
+  private def r(g: GlobalProtocol, xch: String, xt: List[String], pt: Set[String], xi: String): Set[(String, String, List[String])] = {
     println("Rcv(G," + xt + "," + pt + ")(" + xi + ")")
     val (lHash, rHash) = g.getHashes
     val e = lHash(xi)
     println(e)
     e match {
-      case Message(x, p, pp, _, _, xp) if (pt contains pp) => r(g, xt, pt, xp)
-      case ParallelJoin(x, xpp, xp) => r(g, xt, pt, xp)
-      case Message(x, p, pp, l, _, xp) if !(pt contains pp) => Set((pp, l, xt)) ++ r(g, xt, pt + pp, xp)
-      case Choice(x, xp, xpp) => r(g, xt, pt, xp) ++ r(g, xt, pt, xpp)
-      case Parallel(x, xp, xpp) => r(g, xt, pt, xp) ++ r(g, xt, pt, xpp)
+      case Message(x, p, pp, _, _, xp) if (pt contains pp) => r(g, xch, xt, pt, xp)
+      case ParallelJoin(x, xpp, xp) => r(g, xch, xt, pt, xp)
+      case Message(x, p, pp, l, _, xp) if !(pt contains pp) => Set((pp, l, xt)) ++ r(g, xch, xt, pt + pp, xp)
+      case Choice(x, xp, xpp) if (x == xch) => Set.empty
+      case Choice(x, xp, xpp) if (x != xch) => r(g, xch, xt, pt, xp) ++ r(g, xch, xt, pt, xpp)
+      case Parallel(x, xp, xpp) => r(g, xch, xt, pt, xp) ++ r(g, xch, xt, pt, xpp)
       case ChoiceJoin(x, xp, xpp) if (xt contains xpp) => Set.empty
       case End(x) => Set.empty
-      case ChoiceJoin(xp, x, xpp) if !(xt contains xpp) => r(g, xt :+ xpp, pt, xpp)
+      case ChoiceJoin(xp, x, xpp) if !(xt contains xpp) => r(g, xch, xt :+ xpp, pt, xpp)
       // This will happen if the rules are incomplete
       case _ => throw new UndefinedReceiverException(g, xt, pt, xi)
     }
