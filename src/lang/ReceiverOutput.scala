a0c5e7dce472bb278c9a665bab9fe5cb774e7596
Gustavo Soto Ridd <gsoto@dcc.uchile.cl>
2014-09-11 09:02:49 +0200

    format

--- src/lang/ReceiverOutput.scala
+++ src/lang/ReceiverOutput.scala
@@ -7,6 +7,7 @@ package lang
  * non-null suffix
  */
 case class ReceiverOutput(s: Set[(String, String, List[String])]) {
+  
   override def equals(c: Any) = c match {
     case ReceiverOutput(sp) => s forall (e1 => sp forall (e2 => (e1, e2) match {
       case ((p1, l1, xt1), (p2, l2, xt2)) if (p1 == p2) => l1 != l2 || nonNullSuffix(xt1, xt2)
@@ -15,9 +16,9 @@ case class ReceiverOutput(s: Set[(String, String, List[String])]) {
     }))
     case _ => false
   }
-  
+
   def isEmpty = s.isEmpty
-  
+
   /**
    * If any of the two list is empty, then they do not share a non-null suffix.
    * If both lists have elements, they share a non-null suffix if the last
