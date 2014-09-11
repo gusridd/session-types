03a90020292cb36773d3329ca852c18e67996d7d
Gustavo Soto Ridd <gsoto@dcc.uchile.cl>
2014-09-11 09:02:28 +0200

    condition at ReceiverOutput class simplified. Added some comments

--- src/lang/ReceiverOutput.scala
+++ src/lang/ReceiverOutput.scala
@@ -11,18 +11,22 @@ case class ReceiverOutput(s: Set[(String, String, List[String])]) {
     case ReceiverOutput(sp) => s forall (e1 => sp forall (e2 => (e1, e2) match {
       case ((p1, l1, xt1), (p2, l2, xt2)) if (p1 == p2) => l1 != l2 || nonNullSuffix(xt1, xt2)
       case ((p1, l1, xt1), (p2, l2, xt2)) if (p1 != p2) => true
-      case _ => false
+      case _ => throw new Exception("")
     }))
     case _ => false
   }
   
   def isEmpty = s.isEmpty
-
+  
+  /**
+   * If any of the two list is empty, then they do not share a non-null suffix.
+   * If both lists have elements, they share a non-null suffix if the last
+   * element is the same.
+   */
   final private def nonNullSuffix(xt1: List[String], xt2: List[String]): Boolean =
     (xt1, xt2) match {
-      case (List(), h2 :: t2) => false
-      case (h1 :: t1, List()) => false
-      case (List(), List()) => false
+      case (List(), _) => false
+      case (_, List()) => false
       case _ => xt1.last == xt2.last
     }
 }
