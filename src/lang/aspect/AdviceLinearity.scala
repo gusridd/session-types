4231ac03a1a2e707f826d7fdd47226c03391a42d
Gustavo Soto Ridd <gsoto@dcc.uchile.cl>
2014-09-15 13:17:53 +0200

    nothing

--- src/lang/aspect/AdviceLinearity.scala
+++ src/lang/aspect/AdviceLinearity.scala
@@ -4,13 +4,24 @@ import lang.GlobalProtocol
 import lang.WFGlobalProtocol
 
 /**
- * An advice A is advice-linear wft a global type G iff G,A comply with the 
+ * An advice A is advice-linear wft a global type G iff G,A comply with the
  * uniqueMsg and for all M such that match(pc,M), adv is single-threaded wrt M.
  */
 object AdviceLinearity {
 
   def apply(g: GlobalProtocol, a: Aspect): Boolean = {
-    false
+    val uniMsg = uniqueMsg(g, a)
+    val sgTh = SgTh(g, a)
+    val r = uniMsg && sgTh
+    if (r) {
+      println("[SUCCESS] AdviceLinearity")
+    } else {
+      println("[FAIL] AdviceLinearity")
+      println("     ] uniMsg: " + uniMsg)
+      println("     ] sgTh: " + sgTh)
+    }
+    r
+
   }
 
 }
--- src/lang/aspect/SgTh.scala
+++ src/lang/aspect/SgTh.scala
@@ -1,11 +1,20 @@
 package lang.aspect
 
 import lang.GlobalProtocol
+import lang.Message
 
+/**
+ * This condition is true, if for all M such that match(pc,M), adv is
+ * single-threaded wrt M
+ */
 object SgTh {
 
-  def apply(g: GlobalProtocol, a: Aspect) = {
-	  
+  def apply(g: GlobalProtocol, a: Aspect): Boolean = {
+    false
+  }
+
+  private[this] def singleThread(m: Message): Boolean = {
+    false
   }
 
 }
